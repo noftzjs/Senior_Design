dcf83c9f44de166e6fd07c435f23d2d303719a22
Jerry Noftz <noftzjs@mail.uc.edu>
2020-10-22 14:23:05 -0400

    Updating project file strutuce to include components and jsx form file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './components/app/App';
 import * as serviceWorker from './serviceWorker';
+import SubmissionForm from './components/submission-component/submission-component';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SubmissionForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
