ea994fff93df3c8772ef753828f88eab7ee08b94
SBertke5 <70550829+SBertke5@users.noreply.github.com>
2021-01-11 19:58:53 -0500

    Create Logingood.jsx

---
+++ Logingood.jsx
@@ -0,0 +1,54 @@
+import React, { Component, useEffect, useState } from "react";
+import {
+     MDBIcon, MDBBadge
+} from "mdbreact";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
+import './landing-page.css';
+import List from './list';
+import withListLoading from './withListLoading';
+import Submissions from '../submission-component/index'
+import topSubmissions from "./topSubs";
+import SimpleModal from "../modal/submit-modal";
+
+class Login extends Component {
+constructor(props){
+  super(props);
+  this.state={
+  username:'',
+  password:''
+  }
+ }
+render() {
+    return (
+      <div>
+        <MuiThemeProvider>
+          <div>
+          <AppBar
+             title="Login"
+           />
+           <TextField
+             hintText="Enter your Username"
+             floatingLabelText="Username"
+             onChange = {(event,newValue) => this.setState({username:newValue})}
+             />
+           <br/>
+             <TextField
+               type="password"
+               hintText="Enter your Password"
+               floatingLabelText="Password"
+               onChange = {(event,newValue) => this.setState({password:newValue})}
+               />
+             <br/>
+             <RaisedButton label="Submit" primary={true} style={style} onClick={(event) => this.handleClick(event)}/>
+         </div>
+         </MuiThemeProvider>
+      </div>
+    );
+  }
+}
+const style = {
+ margin: 15,
+};
+export default Login;
